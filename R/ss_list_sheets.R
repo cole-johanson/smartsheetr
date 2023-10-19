1278f00f8af24383b621a30f8d59de1d75a9f7d5
Jonathan Carroll <github@jcarroll.com.au>
2023-10-19 13:03:39 +1030

    extend to reports endpoint

--- R/ss_list_sheets.R
+++ R/ss_list_sheets.R
@@ -1,12 +1,20 @@
-#' Get a data frame describing the smartsheets available
+#' Get a data frame describing the smartsheets/reports available
 #'
 #' @details
 #' Note that the environment variable SMARTSHEET_API_TOKEN should be defined in order to run this or any
 #' other `smarsheetr` functions.
+#' @param endpoint either the "sheets" or "reports" API endpoint
 #'
 #' @export
-ss_list_sheets <- function() {
-  resp = ss_get(path='sheets')
+ss_list_sheets <- function(endpoint = c("sheets", "reports")) {
+  path = match.arg(endpoint)
+  resp = ss_get(path = path)
   # TODO: Handle pagination
   ss_resp_data_to_dataframe(resp$content$data)
+}
+
+#' @rdname ss_list_sheets
+#' @export
+ss_list_reports <- function() {
+  ss_list_sheets(endpoint = "reports")
 }
