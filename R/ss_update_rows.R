697a3a44d49658413bb0ba8c34e9361ab3cec691
bleonard314 <bleonard@integral-corp.com>
2023-12-29 11:21:35 -0800

    Updated to address first round of reviewer comments from PR #9.

--- R/ss_update_rows.R
+++ R/ss_update_rows.R
@@ -3,16 +3,17 @@
 #' @param ss_id The sheetId, permalink, or name of the Smartsheet sheet to update
 #' @param data A data frame of rows to be updated. Column names should match Smartsheet column names if use_column_names is TRUE.
 #' @param row_ids Optional; a vector of the rowIds corresponding to the rows in the data frame.
-#'                If `NULL`, the row IDs will be obtained from the sheet.
+#'                Defaults to `NULL`. If `NULL`, the row IDs will be obtained from the sheet.
 #' @param row_numbers Optional; a vector of the row numbers corresponding to the rows in the data frame.
+#'                    This argument can be used to set a row_offset (e.g. `row_numbers = 1:nrow(data)+row_offset`).
+#'                    Defaults to `NULL`. If `NULL`, `row_ids` will be used. If not `NULL`, requires that `row_ids` are `NULL`.
 #' @param column_ids Optional; a vector of the columnIds of the smartsheets sheetId.
 #'                   If `NULL` and use_column_names is TRUE, the column IDs will be matched based on the column names in `data`.
-#' @param row_offset The number of rows to skip from the beginning for updating.
-#'                   Defaults to 0 (start at the first row).
-#' @param column_offset The number of columns to skip from the beginning for updating.
+#' @param column_offset Optional; The number of columns to skip from the beginning for updating.
 #'                      Defaults to 0 (start at the first column).
-#' @param use_column_names Logical; if TRUE, column names in `data` are used to match column IDs in the sheet.
-#'                         Defaults to TRUE.
+#'                      If non-zero, requires that `column_ids` are `NULL` and `use_column_names` is `FALSE`.
+#' @param use_column_names Optional; If `TRUE`, column names in `data` are used to match column IDs in the sheet.
+#'                         Defaults to `TRUE`.
 #'
 #' @examples
 #' \dontrun{
@@ -28,24 +29,29 @@
 #' @return A `ss_updaterows_resp` object
 #'
 #' @export
-ss_update_rows <- function(ss_id, data, row_ids = NULL, row_numbers = NULL, column_ids = NULL, row_offset = 0, column_offset = 0, use_column_names = TRUE) {
+ss_update_rows <- function(ss_id, data, row_ids = NULL, row_numbers = NULL, column_ids = NULL, column_offset = 0, use_column_names = TRUE) {
   ss_id = validate_ss_id(ss_id)
   if (!is.data.frame(data) || nrow(data) == 0) {
-    stop("data must be a non-empty data frame.")
+    rlang::abort("data must be a non-empty data frame.")
   }
   if (!is.null(row_numbers) && (!is.numeric(row_numbers) || any(row_numbers < 1))) {
-    stop("row_numbers must be a vector of positive numbers.")
+    rlang::abort("row_numbers must be a vector of positive numbers.")
   }
-  if (length(row_numbers) != nrow(data)) {
-    stop("row_numbers must be the same length as the data.")
+  if (!is.null(row_numbers) && (length(row_numbers) != nrow(data))) {
+    rlang::abort("row_numbers must be the same length as the data.")
   }
-  if (!is.null(row_offset) && (!is.numeric(row_offset) || row_offset < 0)) {
-    stop("row_offset must be a non-negative number.")
+  if (!is.null(row_numbers) && !is.null(row_ids)) {
+    rlang::abort("row_numbers cannot be used with row_ids.")
   }
   if (!is.null(column_offset) && (!is.numeric(column_offset) || column_offset < 0)) {
-    stop("column_offset must be a non-negative number.")
+    rlang::abort("column_offset must be a non-negative number.")
   }
-
+  if (column_offset != 0 && !is.null(column_ids)) {
+    rlang::abort("column_offset cannot be used with column_ids.")
+  }
+  if (column_offset != 0 && use_column_names) {
+    rlang::abort("column_offset cannot be used with use_column_names.")
+  }
   # Fetching Column IDs
   if(is.null(column_ids)) {
     col_resp = ss_get(path = paste0('sheets/', ss_id, '/columns'))
@@ -58,11 +64,11 @@ ss_update_rows <- function(ss_id, data, row_ids = NULL, row_numbers = NULL, colu
       data_column_names = names(data)
       unmatched_columns = setdiff(data_column_names, sheet_columns$title)
       if (length(unmatched_columns) > 0) {
-        stop("No match found for column names: ", paste(unmatched_columns, collapse = ", "))
+        rlang::abort("No match found for column names: ", paste(unmatched_columns, collapse = ", "))
       }
       column_ids = sheet_columns |>
-        dplyr::filter(title %in% data_column_names) |>
-        dplyr::pull(id)
+        dplyr::filter(rlang::.data$title %in% data_column_names) |>
+        dplyr::pull(rlang::.data$id)
     } else {
       # Use all column IDs from the sheet
       column_ids = purrr::map(col_resp$content$data, ~.x$id) |> purrr::list_c()
@@ -74,12 +80,12 @@ ss_update_rows <- function(ss_id, data, row_ids = NULL, row_numbers = NULL, colu
     # Fetching the number of rows in the data
     if (is.null(row_numbers)) {
       row_seq = 1:nrow(data)
-      row_numbers = row_seq+row_offset
+      row_numbers = row_seq
     }
     # Convert row numbers to comma separated string
     row_numbers = paste(row_numbers, collapse = ",")
     if (nchar(row_numbers) >= 5000) {
-      warning("The number of rows to update is too large. Please use row_ids instead.")
+      rlang::warn("The number of rows to update is too large. Please use row_ids instead.")
       row_query = list()
     } else {
       row_query = list(rowNumbers=row_numbers)
@@ -88,19 +94,19 @@ ss_update_rows <- function(ss_id, data, row_ids = NULL, row_numbers = NULL, colu
     row_ids = purrr::map(row_resp$content$rows, ~.x$id) |> purrr::list_c()
     # Check if enough row IDs are available
     if (length(row_ids) == 0) {
-      stop("No row IDs available to update. Please add rows to the sheet.")
+      rlang::abort("No row IDs available to update. Please add rows to the sheet.")
     }
     if (length(row_ids) < nrow(data)) {
-      stop("Not enough row IDs available to update all rows.")
+      rlang::abort("Not enough row IDs available to update all rows.")
     }
   }
 
   if(length(row_ids) != nrow(data)) {
-    stop("The number of row_ids must match the number of rows in the data frame.")
+    rlang::abort("The number of row_ids must match the number of rows in the data frame.")
   }
 
   if(column_offset >= ncol(data)) {
-    stop("The column_offset must be less than the number of columns in the data frame.")
+    rlang::abort("The column_offset must be less than the number of columns in the data frame.")
   }
 
   # Format row data for API payload
@@ -110,8 +116,7 @@ ss_update_rows <- function(ss_id, data, row_ids = NULL, row_numbers = NULL, colu
     for(j in (1 + column_offset):ncol(data)) {
       cell_value = unlist(data[i, j])
       if (is.na(cell_value)) {
-        # Convert NaNs to NA
-        cell_value = NA
+        cell_value = NA # Convert NaNs to NA (otherwise this is converted to text)
       }
       cell_data[[j - column_offset]] = list(columnId = unlist(column_ids[j]), value = cell_value)
     }
